dc2903732939b98a74f3dd992ad131560d12fd90
Ahri Earl Spilner <ozogtea07@gmail.com>
2020-10-23 20:46:54 +0700

    Tugas 2 JS

---
+++ index.js
@@ -0,0 +1,21 @@
+var text1 ="aku";
+var text2 = 20;
+var cm = 30;
+var text4 = "ada";
+var text5;
+var text6 = [1,2,"aku",true];
+
+if(cm>40)
+text3 = true;
+else
+text3 = false;
+
+text4 = null;
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
+console.log(text4);
+console.log(text5);
+console.log(text6);
+console.log(text6[2]);
